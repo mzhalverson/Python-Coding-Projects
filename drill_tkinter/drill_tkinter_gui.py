5b6bc13044f1618160ff7c6511a933b90a0b81a1
zechlin <zechlinmj@gmail.com>
2019-09-02 09:38:16 -0700

    Remove and Rename extra

--- drill_tkinter/drill_tkinter_gui.py
+++ drill_tkinter/drill_tkinter_gui.py
@@ -7,15 +7,15 @@ import drill_tkinter_func
 
 def load_gui(self):
     
-    self.varFile1 = StringVar()
-    self.varFile2 = StringVar()
+    self.varDir1 = StringVar()
+    self.varDir2 = StringVar()
 
 
-    self.txtFile1 = Entry(self.master, text=self.varFile1, font=("Helvetica", 13), fg='black', bg='white', width=33)
-    self.txtFile1.grid(row=0, column=1, padx=(40,0), pady=(50,0))
+    self.txtDir1 = Entry(self.master, text=self.varDir1, font=("Helvetica", 13), fg='black', bg='white', width=33)
+    self.txtDir1.grid(row=0, column=1, padx=(40,0), pady=(50,0))
 
-    self.txtFile2 = Entry(self.master, text=self.varFile2, font=("Helvetica", 13), fg='black', bg='white', width=33)
-    self.txtFile2.grid(row=1, column=1,  padx=(40,0), pady=(20,0))
+    self.txtDir2 = Entry(self.master, text=self.varDir2, font=("Helvetica", 13), fg='black', bg='white', width=33)
+    self.txtDir2.grid(row=1, column=1,  padx=(40,0), pady=(20,0))
 
     self.btnBrowse = Button(self.master, text="Browse...", width=13, height=1, command=lambda: drill_tkinter_func.findDir1(self))
     self.btnBrowse.grid(row=0, column=0,  padx=(20,0), pady=(50,0))

ab1fd10a0f5bb12c4daf3e9a847ebbddab0ff73e
zechlin <zechlinmj@gmail.com>
2019-09-02 10:39:09 -0700

    Final Drill commit

--- drill_tkinter/drill_tkinter_func.py
+++ drill_tkinter/drill_tkinter_func.py
@@ -1,9 +1,10 @@
 
 import os
+import sqlite3
 from tkinter import *
 import tkinter as tk
-from tkinter import messagebox
-
+from tkinter import filedialog
+import shutil
 
 # Be sure to import our other modules
 # so we can have access to them
@@ -11,14 +12,52 @@ import drill_tkinter_main
 import drill_tkinter_gui
 
 # Ask Directory
-def findDir1(self):
+def findSource(self):
     filePath = tk.filedialog.askdirectory()
-    self.txtDir1.insert(0,filePath)
+    self.txtSource.insert(0,filePath)
 
-def findDir2(self):
+def findDestination(self):
     filePath = tk.filedialog.askdirectory()
-    self.txtDir2.insert(0,filePath)
+    self.txtDestination.insert(0,filePath)
     
+def checkFiles(self):
+
+    var_source = self.txtSource.get()
+    var_destination = self.txtDestination.get()
+    
+    if (len(var_source) > 0) and (len(var_destination) > 0) : # enforce the user to provide both names
+        conn = sqlite3.connect('moved_files.db')
+        with conn:
+            cursor = conn.cursor()
+
+            file_list = os.listdir(var_source)
+            for file in file_list:
+                name, ext = os.path.splitext(file)        
+                if ext == '.txt':
+                    abspath = os.path.join(var_source, file)
+                    last_rev = os.path.getmtime(abspath)
+                    print(abspath, '\n', last_rev)
+
+                    cursor.execute("""INSERT INTO tbl_files (col_file, col_time_stamp) VALUES (?,?)""",(file,last_rev))
+                    shutil.move(abspath, var_destination)
+        conn.commit()
+        conn.close()
+    else:
+        messagebox.showerror("Missing Text Error","Please ensure that there is data in all four fields.")
+
+
+def create_db(self):
+    conn = sqlite3.connect('moved_files.db')
+    with conn:
+        cur = conn.cursor()
+        cur.execute("CREATE TABLE if not exists tbl_files( \
+            ID INTEGER PRIMARY KEY AUTOINCREMENT, \
+            col_file TEXT, \
+            col_time_stamp TEXT \
+            );")
+        # You must commit() to save changes & close th database connection
+        conn.commit()
+    conn.close()
 
 
 if __name__ == "__main__":
--- drill_tkinter/drill_tkinter_gui.py
+++ drill_tkinter/drill_tkinter_gui.py
@@ -7,28 +7,29 @@ import drill_tkinter_func
 
 def load_gui(self):
     
-    self.varDir1 = StringVar()
-    self.varDir2 = StringVar()
+    self.varSource = StringVar()
+    self.varDestination = StringVar()
 
 
-    self.txtDir1 = Entry(self.master, text=self.varDir1, font=("Helvetica", 13), fg='black', bg='white', width=33)
-    self.txtDir1.grid(row=0, column=1, padx=(40,0), pady=(50,0))
+    self.txtSource = Entry(self.master, text=self.varSource, font=("Helvetica", 13), fg='black', bg='white', width=33)
+    self.txtSource.grid(row=0, column=1, padx=(40,0), pady=(50,0))
 
-    self.txtDir2 = Entry(self.master, text=self.varDir2, font=("Helvetica", 13), fg='black', bg='white', width=33)
-    self.txtDir2.grid(row=1, column=1,  padx=(40,0), pady=(20,0))
+    self.txtDestination = Entry(self.master, text=self.varDestination, font=("Helvetica", 13), fg='black', bg='white', width=33)
+    self.txtDestination.grid(row=1, column=1,  padx=(40,0), pady=(20,0))
 
-    self.btnBrowse = Button(self.master, text="Browse...", width=13, height=1, command=lambda: drill_tkinter_func.findDir1(self))
+    self.btnBrowse = Button(self.master, text="Browse...", width=13, height=1, command=lambda: drill_tkinter_func.findSource(self))
     self.btnBrowse.grid(row=0, column=0,  padx=(20,0), pady=(50,0))
 
-    self.btnBrowse = Button(self.master, text="Browse...", width=13, height=1, command=lambda: drill_tkinter_func.findDir2(self))
+    self.btnBrowse = Button(self.master, text="Browse...", width=13, height=1, command=lambda: drill_tkinter_func.findDestination(self))
     self.btnBrowse.grid(row=1, column=0,  padx=(20,0), pady=(20,0))
 
-    self.btnCheck = Button(self.master, text="Check for files...", width=13, height=2)
+    self.btnCheck = Button(self.master, text="Check for files...", width=13, height=2, command=lambda: drill_tkinter_func.checkFiles(self))
     self.btnCheck.grid(row=2, column=0,  padx=(20,0), pady=(20,0))
 
     self.btnCancel = Button(self.master, text="Close Program", width=13, height=2)
     self.btnCancel.grid(row=2, column=1,  padx=(0,0), pady=(20,0), sticky=NE)
 
+    drill_tkinter_func.create_db(self)
 
 if __name__ == "__main__":
     pass
